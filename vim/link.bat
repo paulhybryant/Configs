6991fbe69112efbb5c3329471ee374bbe47cf2d0
paulhybryant <paulhybryant@gmail.com>
2014-12-02 12:07:02 -0800

    .

--- vim/link.bat
+++ vim/link.bat
@@ -2,7 +2,7 @@ set NEWPROFILE="D:\Users\Yu"
 
 mklink %USERPROFILE%\.vimrc %~dp0%\.vimrc
 mklink %USERPROFILE%\.gvimrc %~dp0%\.gvimrc
-mklink %USERPROFILE%\.vimrc.local %~dp0%\.vimrc.nongoogle
+mklink %USERPROFILE%\.vimrc.local %~dp0%\.vimrc.personal
 mklink %USERPROFILE%\.gitconfig %~dp0%\..\misc\.gitconfig-windows
 mklink /J %USERPROFILE%\Notes %~dp0%\..\notes
 mklink /J %USERPROFILE%\.ctagscnf %~dp0%\..\ctags
