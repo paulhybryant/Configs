173087f06ef149734262d768f0d34d2ce8a2e085
paulhybryant <paulhybryant@gmail.com>
2014-12-25 16:09:06 -0800

    .

---
+++ utils/bash/winmove.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+DEBUG=true
+LOGFILE=/tmp/log.txt
+
+function get_active_win_geometry() {
+  local _id_=$(xdotool getactivewindow)
+  while read -a a;
+    do
+      w=${a[0]};
+      if (($((16#${w:2}))==_id_)); then
+        WIN_X=${a[3]}
+        WIN_Y=${a[4]}
+        WIN_W=${a[5]}
+        WIN_H=${a[6]}
+        if [[ $DEBUG == true ]]; then
+          echo $WIN_X $WIN_Y $WIN_W $WIN_H >> $LOGFILE
+        fi
+        break;
+      fi;
+    done < <(wmctrl -lpG)
+}
+
+function log_run() {
+  if [[ $DEBUG == true ]]; then
+    echo "$1" >> $LOGFILE
+  fi
+  $1
+}
+
+get_active_win_geometry
+if [[ $DEBUG == true ]]; then
+  echo "Moving to $1" >> $LOGFILE
+fi
+
+if [[ $WIN_X -ge 1366 ]]; then
+  if [[ $1 == "LEFT" ]]; then
+    log_run 'wmctrl -r :ACTIVE: -e 0,0,0,1366,700'
+  fi
+else
+  if [[ $1 == "RIGHT" ]]; then
+    log_run 'wmctrl -r :ACTIVE: -e 0,1366,0,1920,1160'
+  fi
+fi
