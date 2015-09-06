f20467f15a42e02b3feefc3786ce72015fd6f9c6
paulhybryant <paulhybryant@gmail.com>
2015-09-06 11:01:26 +0800

    .

--- shlib/git.zsh
+++ shlib/git.zsh
@@ -31,3 +31,25 @@ function git::has_branch() {
   [[ -n $(git branch --list "$1") ]] && return 0
   return 1
 }
+
+# Get parent branch of a branch, defaults to current branch.
+# How it works:
+# 1| Display a textual history of all commits.
+# 2| Ancestors of the current commit are indicated
+#    by a star. Filter out everything else.
+# 3| Ignore all the commits in the current branch.
+# 4| The first result will be the nearest ancestor branch.
+#    Ignore the other results.
+# 5| Branch names are displayed [in brackets]. Ignore
+#    everything outside the brackets, and the brackets.
+# 6| Sometimes the branch name will include a ~2 or ^1 to
+#    indicate how many commits are between the referenced
+#    commit and the branch tip. We don't care. Ignore them.
+function git::parent_branch() {
+  if [[ $# -eq 0 ]]; then
+    local _branch=$(git rev-parse --abbrev-ref HEAD)
+  else
+    local _branch=$1
+  fi
+  git show-branch | ack '\*' | ack -v "$_branch" | head -n1 | sed 's/.*\[\(.*\)\].*/\1/' | sed 's/[\^~].*//'
+}
