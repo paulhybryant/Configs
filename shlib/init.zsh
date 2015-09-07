a9e50c8a6aa9b0ee6ecdfe4e575e20af8bd6868d
paulhybryant <paulhybryant@gmail.com>
2015-09-07 17:06:45 +0800

    .

--- shlib/init.zsh
+++ shlib/init.zsh
@@ -1,7 +1,40 @@
 # vim: filetype=zsh sw=2 ts=2 sts=2 et tw=80 foldlevel=0 nospell
 
-# Library include guard
-# $1 library script path
+: <<=cut
+=pod
+=head1 NAME
+
+init.zsh - Library source guard.
+
+=head1 SYNOPSIS
+
+    Put the following at the start of all library files.
+
+    source init.zsh
+    init::sourced "${0:a}" && return
+
+=head1 DESCRIPTION
+
+Library source guard.
+
+=head2 Methods
+
+=over 12
+
+=item C<init::sourced>
+
+Returns a status indicating whether the library file is already sourced and have
+not changed since then.
+
+=back
+
+=head1 LICENSE
+
+=head1 AUTHOR
+
+Yu Huang
+
+=cut
 function init::sourced() {
   # strip the .zsh extension
   local _var="__SOURCED_${${1:t:u}%.ZSH}__"
