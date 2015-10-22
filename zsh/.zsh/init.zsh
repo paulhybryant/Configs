b26a024079aace2f0ca6e675c89bf869007eb6e9
paulhybryant <paulhybryant@gmail.com>
2015-10-22 13:16:34 +0800

    .

--- zsh/.zsh/init.zsh
+++ zsh/.zsh/init.zsh
@@ -1,91 +1,5 @@
 # vim: filetype=zsh sw=2 ts=2 sts=2 et tw=80 foldlevel=0 nospell
 
-# Options are not ordered alphabetically, but their order in zsh man page
-# Changing Directories
-setopt AUTOCD                                                                  # Switching directories for lazy people
-setopt AUTO_PUSHD
-setopt PUSHD_IGNORE_DUPS
-setopt PUSHD_MINUS
-setopt PUSHD_SILENT
-
-# Completion
-setopt ALWAYS_TO_END                                                            # When complete from middle, move cursor
-setopt AUTO_LIST
-setopt AUTO_MENU                                                                # Automatically use menu completion after the second consecutive request for completion
-setopt AUTO_PARAM_KEYS
-setopt AUTO_PARAM_SLASH
-setopt AUTO_REMOVE_SLASH
-setopt COMPLETE_ALIASES                                                         # Prevent aliases from being internally substituted before completion is attempted
-setopt COMPLETE_IN_WORD                                                         # Not just at the end
-setopt GLOB_COMPLETE
-setopt LIST_AMBIGUOUS
-setopt LIST_TYPES
-setopt MENU_COMPLETE
-
-# Expansion and Globbing
-setopt BAD_PATTERN
-setopt CASEMATCH                                                                # Whether the regex comparison (e.g. =~) will match case
-setopt EXTENDED_GLOB                                                            # Weird &amp; wacky pattern matching - yay zsh!
-setopt GLOB
-setopt MARK_DIRS
-setopt NO_NOMATCH                                                               # pass through '*' if globbing fails
-setopt WARN_CREATE_GLOBAL
-
-# History
-setopt APPEND_HISTORY
-setopt BANG_HIST
-setopt EXTENDED_HISTORY
-setopt HIST_ALLOW_CLOBBER
-setopt HIST_EXPIRE_DUPS_FIRST
-setopt HIST_FCNTL_LOCK
-setopt HIST_FIND_NO_DUPS
-setopt HIST_IGNORE_ALL_DUPS                                                     # Do not enter command lines into the history list if they are duplicates of the previous event
-setopt HIST_IGNORE_DUPS                                                         # ignore duplication command history list
-setopt HIST_IGNORE_SPACE                                                        # Remove command lines from the history list when the first character on the line is a space
-setopt HIST_NO_STORE
-setopt HIST_REDUCE_BLANKS                                                       # Remove superfluous blanks from each command line being added to the history list
-setopt HIST_SAVE_NO_DUPS
-setopt HIST_VERIFY                                                              # When using ! cmds, confirm first
-setopt INC_APPEND_HISTORY
-setopt SHARE_HISTORY                                                            # share command history data
-
-# Input/Output
-setopt ALIASES
-setopt CLOBBER
-setopt CORRECT
-setopt NO_FLOW_CONTROL
-setopt INTERACTIVE_COMMENTS                                                     # Escape commands so I can use them later
-setopt PRINT_EXIT_VALUE                                                         # Alert me if something's failed
-setopt RC_QUOTES
-setopt SHORT_LOOPS
-
-# Job Control
-setopt CHECK_JOBS
-setopt NOHUP                                                                    # Don't kill background jobs when I logout
-setopt LONG_LIST_JOBS
-setopt MONITOR
-
-# Prompting
-setopt PROMPT_BANG
-setopt PROMPT_CR                                                             # Default on, resulting in a carriage return ^M when enter on the numeric pad is pressed.
-setopt PROMPT_PERCENT
-setopt PROMPT_SUBST
-
-# Scripts and Functions
-setopt C_BASES
-setopt ERR_RETURN
-setopt FUNCTION_ARGZERO
-# setopt SOURCE_TRACE
-# setopt XTRACE
-
-# Shell Emulation
-setopt NO_CONTINUE_ON_ERROR
-# setopt KSH_ARRAYS                                                               # Make array index starts at 0
-setopt RC_EXPAND_PARAM
-
-# Zle
-setopt VI                                                                       # Use vi key bindings in ZSH (bindkey -v)
-
 case $HIST_STAMPS in
   'mm/dd/yyyy') alias history='fc -fl 1' ;;
   'dd.mm.yyyy') alias history='fc -El 1' ;;
@@ -164,3 +78,89 @@ util::fix-display
 util::start-ssh-agent "${SSH_AGENT_NAME}"
 
 alias ssh='net::ssh'
+
+# Options are not ordered alphabetically, but their order in zsh man page
+# Changing Directories
+setopt AUTOCD                                                                  # Switching directories for lazy people
+setopt AUTO_PUSHD
+setopt PUSHD_IGNORE_DUPS
+setopt PUSHD_MINUS
+setopt PUSHD_SILENT
+
+# Completion
+setopt ALWAYS_TO_END                                                            # When complete from middle, move cursor
+setopt AUTO_LIST
+setopt AUTO_MENU                                                                # Automatically use menu completion after the second consecutive request for completion
+setopt AUTO_PARAM_KEYS
+setopt AUTO_PARAM_SLASH
+setopt AUTO_REMOVE_SLASH
+setopt COMPLETE_ALIASES                                                         # Prevent aliases from being internally substituted before completion is attempted
+setopt COMPLETE_IN_WORD                                                         # Not just at the end
+setopt GLOB_COMPLETE
+setopt LIST_AMBIGUOUS
+setopt LIST_TYPES
+setopt MENU_COMPLETE
+
+# Expansion and Globbing
+setopt BAD_PATTERN
+setopt CASEMATCH                                                                # Whether the regex comparison (e.g. =~) will match case
+setopt EXTENDED_GLOB                                                            # Weird &amp; wacky pattern matching - yay zsh!
+setopt GLOB
+setopt MARK_DIRS
+setopt NO_NOMATCH                                                               # pass through '*' if globbing fails
+# setopt WARN_CREATE_GLOBAL
+
+# History
+setopt APPEND_HISTORY
+setopt BANG_HIST
+setopt EXTENDED_HISTORY
+setopt HIST_ALLOW_CLOBBER
+setopt HIST_EXPIRE_DUPS_FIRST
+setopt HIST_FCNTL_LOCK
+setopt HIST_FIND_NO_DUPS
+setopt HIST_IGNORE_ALL_DUPS                                                     # Do not enter command lines into the history list if they are duplicates of the previous event
+setopt HIST_IGNORE_DUPS                                                         # ignore duplication command history list
+setopt HIST_IGNORE_SPACE                                                        # Remove command lines from the history list when the first character on the line is a space
+setopt HIST_NO_STORE
+setopt HIST_REDUCE_BLANKS                                                       # Remove superfluous blanks from each command line being added to the history list
+setopt HIST_SAVE_NO_DUPS
+setopt HIST_VERIFY                                                              # When using ! cmds, confirm first
+setopt INC_APPEND_HISTORY
+setopt SHARE_HISTORY                                                            # share command history data
+
+# Input/Output
+setopt ALIASES
+setopt CLOBBER
+setopt CORRECT
+setopt NO_FLOW_CONTROL
+setopt INTERACTIVE_COMMENTS                                                     # Escape commands so I can use them later
+setopt PRINT_EXIT_VALUE                                                         # Alert me if something's failed
+setopt RC_QUOTES
+setopt SHORT_LOOPS
+
+# Job Control
+setopt CHECK_JOBS
+setopt NOHUP                                                                    # Don't kill background jobs when I logout
+setopt LONG_LIST_JOBS
+setopt MONITOR
+
+# Prompting
+setopt PROMPT_BANG
+setopt PROMPT_CR                                                                # Default on, resulting in a carriage return ^M when enter on the numeric pad is pressed.
+setopt PROMPT_PERCENT
+setopt PROMPT_SUBST
+
+# Scripts and Functions
+setopt C_BASES
+# setopt ERR_RETURN                                                             # Enable this would break completion!
+setopt FUNCTION_ARGZERO
+# setopt SOURCE_TRACE
+# setopt XTRACE
+
+# Shell Emulation
+setopt NO_CONTINUE_ON_ERROR
+# setopt KSH_ARRAYS                                                               # Make array index starts at 0
+setopt RC_EXPAND_PARAM
+
+# Zle
+setopt VI                                                                       # Use vi key bindings in ZSH (bindkey -v)
