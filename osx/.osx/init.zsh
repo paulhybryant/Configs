0c65ddd83cffdd873b5733d6502c7b076adfd720
paulhybryant <paulhybryant@gmail.com>
2015-11-16 09:33:42 +0800

    .

--- osx/.osx/init.zsh
+++ osx/.osx/init.zsh
@@ -12,3 +12,7 @@ defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadCorner
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadRightClick -bool true
 defaults -currentHost write NSGlobalDomain com.apple.trackpad.trackpadCornerClickBehavior -int 1
 defaults -currentHost write NSGlobalDomain com.apple.trackpad.enableSecondaryClick -bool true
+
+./karabiner-import.sh
+./seil-import.sh
+./spectacle-import.sh
