65a58b1c72f58ae0ca0ec91989953bfe0ed19f4b
paulhybryant <paulhybryant@gmail.com>
2015-11-18 14:15:08 +0800

    .

--- misc/.local/bin/geoinfo.js
+++ misc/.local/bin/geoinfo.js
@@ -1,11 +1,29 @@
 #!/usr/bin/env node
 
 var http = require('http');
+// Need to npm link commander to make it available
+var program = require('commander');
 
-var options = {
-  host: 'int.dpool.sina.com.cn',
-  path: '/iplookup/iplookup.php?format=json'
-};
+program
+  .version('0.0.1')
+  .option('-n, --nocache', 'Do not use cache')
+  .option('-e, --nocn', 'Do not use sites in China to get location')
+  .parse(process.argv);
+
+if (program.nocache)
+  console.log('nocache');
+
+if (program.nocn) {
+  var options = {
+    host: 'ipinfo.io',
+    path: ''
+  };
+} else {
+  var options = {
+    host: 'int.dpool.sina.com.cn',
+    path: '/iplookup/iplookup.php?format=json'
+  };
+}
 
 callback = function(response) {
   var str = '';
@@ -17,7 +35,8 @@ callback = function(response) {
 
   //the whole response has been recieved, so we just print it out here
   response.on('end', function () {
-    console.log(str);
+    fs = require('fs');
+    fs.writeFile('/tmp/.util.geoinfo', str)
   });
 }
 
