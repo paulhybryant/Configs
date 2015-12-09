df75eebba262a687d531c0cc4e983bb8d1002920
paulhybryant <paulhybryant@gmail.com>
2015-12-09 13:27:02 +0800

    .

--- zsh/.zsh/tests/shell/zsh::autoload.zsh
+++ zsh/.zsh/tests/shell/zsh::autoload.zsh
@@ -10,19 +10,19 @@ set -x
 function test::zsh::autoload() {
   local _expected
   _expected=$(time::getmtime ${_dir}/zsh::autoload)
-  zstyle -s ":fnregistry:zsh::autoload" fnregistry "_actual"
+  zstyle -s ":registry:fn:zsh::autoload" registry "_actual"
   [[ "${_expected}" == "${_actual}" ]]
 
   touch ${_dir}/zsh::autoload
   zsh::autoload -d ${_dir} zsh::autoload
   _expected=$(time::getmtime ${_dir}/zsh::autoload)
-  zstyle -s ":fnregistry:zsh::autoload" fnregistry "_actual"
+  zstyle -s ":registry:fn:zsh::autoload" registry "_actual"
   [[ "${_expected}" == "${_actual}" ]]
 
   touch ${_dir}/zsh::autoload
   zsh::autoload -a -d ${_dir}
   _expected=$(time::getmtime ${_dir}/zsh::autoload)
-  zstyle -s ":fnregistry:zsh::autoload" fnregistry "_actual"
+  zstyle -s ":registry:fn:zsh::autoload" registry "_actual"
   [[ "${_expected}" == "${_actual}" ]]
 }
 test::zsh::autoload
