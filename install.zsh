2a2a55bc34783c2b17bb1d0fa16e306c0d2efb67
paulhybryant <paulhybryant@gmail.com>
2015-12-22 13:29:30 +0800

    .

--- install.zsh
+++ install.zsh
@@ -18,20 +18,20 @@ zparseopts -D -K -M -E -- h=help v=verbose \
 local log
 log=$(mktemp)
 
-printf "Sourcing antigen...\n" | tee "${log}"
+printf 'Sourcing antigen...\n' | tee "${log}"
 source <(curl -sL https://raw.githubusercontent.com/zsh-users/antigen/master/antigen.zsh) || return 1
 
 [[ -n ${verbose} ]] && setopt verbose
-local url="$(-antigen-resolve-bundle-url "https://github.com/paulhybryant/dotfiles.git")"
+local url="$(-antigen-resolve-bundle-url 'https://github.com/paulhybryant/dotfiles.git')"
 -antigen-ensure-repo "${url}"
 pushd $(-antigen-get-clone-dir "${url}")
-git remote set-url origin "git@github.com:paulhybryant/dotfiles.git"
+git remote set-url origin 'git@github.com:paulhybryant/dotfiles.git'
 ./zsh/.zsh/bin/bootstrap --dryrun | tee -a "${log}"
-printf "Continue [y/n]? "
+printf 'Continue [y/n]? '
 read -r reply
 case $reply in
   Y*|y*)
-    printf "Bootstraping...\n" | tee -a "${log}"
+    printf 'Bootstraping...\n' | tee -a "${log}"
     ./zsh/.zsh/bin/bootstrap --log "${log}" | tee -a "${log}"
     ;;
   *)
