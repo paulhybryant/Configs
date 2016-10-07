ba389a4bbc2e3c5c2b8b9ea47ec88db952085d08
paulhybryant <paulhybryant@gmail.com>
2016-10-07 22:18:40 +0800

    .

--- zsh/.zplug.zsh
+++ zsh/.zplug.zsh
@@ -2,3 +2,6 @@ export ZPLUG_HOME="$BREWHOME/opt/zplug"
 source ${ZPLUG_HOME}/init.zsh
 
 zplug "zsh-users/zsh-history-substring-search"
+zplug "paulhybryant/powerline-shell", use:"powerline-shell.zsh"
+
+zplug load
