9335a8b8b8cb8051696841c4085430a96f3dc4a1
paulhybryant <paulhybryant@gmail.com>
2016-10-09 16:33:20 +0800

    .

--- zsh/.zplug.zsh
+++ zsh/.zplug.zsh
@@ -3,5 +3,10 @@ source ${ZPLUG_HOME}/init.zsh
 
 zplug "zsh-users/zsh-history-substring-search"
 zplug "paulhybryant/powerline-shell", use:"powerline-shell.zsh"
+zplug "junegunn/fzf", use:"shell/*.zsh"
+# zplug "andrewferrier/fzf-z"
+# zplug "modules/fasd", from:prezto
+
+zplug "$HOME/.gitrepo/paulhybryant/GoogleConfigs/google.plugin.zsh", from:local
 
 zplug load
