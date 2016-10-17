0cb4bf7ec6a74c91b1c1253f017d33ee3f3b45da
paulhybryant <paulhybryant@gmail.com>
2016-10-17 11:26:38 +0800

    .

--- zsh/.antigen.zsh
+++ zsh/.antigen.zsh
@@ -1,7 +1,7 @@
 source ~/.antigen/repos/antigen/antigen.zsh
 antigen use prezto                                                              # ZDOTDIR is set here
 # mollifier/anyframe mollifier/zload uvaes/fzf-marks mafredri/zsh-async
-antigen bundle Tarrasch/zsh-colors
+# antigen bundle Tarrasch/zsh-colors
 [[ $OSTYPE == *darwin* ]] && \
   antigen bundle unixorn/tumult.plugin.zsh && unalias vi && unalias vim
 # antigen bundle hchbaw/auto-fu.zsh
--- zsh/.zplug.zsh
+++ zsh/.zplug.zsh
@@ -2,18 +2,11 @@ export ZPLUG_HOME="$BREWHOME/opt/zplug"
 export ZPLUG_REPOS="$HOME/.zplug/repos"
 source ${ZPLUG_HOME}/init.zsh
 
-zstyle ":prezto:environment:termcap" "color" "yes"
-zplug "modules/environment", from:prezto, nice:0
-zplug "modules/directory", from:prezto, nice:1
 zplug "modules/helper", from:prezto, nice:1
-zstyle ":prezto:module:editor" "key-bindings" "vi"
-zplug "modules/editor", from:prezto, nice:1
-zplug "modules/git", from:prezto, nice:1
-zplug "modules/homebrew", from:prezto, nice:1
-zplug "modules/fasd", from:prezto, nice:1
+# zstyle ":prezto:module:editor" "key-bindings" "vi"
+# zplug "modules/editor", from:prezto, nice:1
+# zplug "modules/ssh", from:prezto, nice:1
 zplug "modules/history", from:prezto, nice:1
-zplug "modules/tmux", from:prezto, nice:1
-zplug "modules/dpkg", from:prezto, nice:1
 zplug "modules/osx", from:prezto, nice:1, if:"[[ $OSTYPE == *darwin* ]]"
 
 # For unknown reason, zplug clear && zplug load will cause zsh to exit if
@@ -25,12 +18,12 @@ zplug "RobSis/zsh-completion-generator", nice:6
 zplug "psprint/ztrace", nice:6
 zplug "psprint/zsnapshot", nice:6
 zplug "jocelynmallon/zshmarks", nice:6
-# zplug "sharat87/zsh-vim-mode", nice:6
+zplug "sharat87/zsh-vim-mode", nice:6
 zplug "seebi/dircolors-solarized", nice:6
 zplug "paulhybryant/powerline-shell", nice:6
 zplug "junegunn/fzf", use:"shell/*.zsh", nice:6
 zplug "zlsun/solarized-man", nice:6
-zplug "andrewferrier/fzf-z", nice:6
+# zplug "andrewferrier/fzf-z", nice:6
 zplug "unixorn/tumult.plugin.zsh", nice:6, if:"[[ $OSTYPE == *darwin* ]]"
 # zplug "b4b4r07/enhancd", use:"init.sh", nice:7
 zplug "paulhybryant/myzsh", use:"enabled", nice:8
