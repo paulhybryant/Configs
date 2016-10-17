8ec88d1fe81753b7292f5abd7a31a51765ca568d
paulhybryant <paulhybryant@gmail.com>
2016-10-17 17:25:10 +0800

    .

--- zsh/.zplug.zsh
+++ zsh/.zplug.zsh
@@ -2,6 +2,8 @@ export ZPLUG_HOME="$BREWHOME/opt/zplug"
 export ZPLUG_REPOS="$HOME/.zplug/repos"
 source ${ZPLUG_HOME}/init.zsh
 
+zplug "paulhybryant/dotfiles", as:command, use:"misc/.local/bin/{xclipper,save2tmp}", nice:0
+
 zstyle ":prezto:environment:termcap" "color" "yes"
 zplug "modules/environment", from:prezto, nice:0
 zplug "modules/directory", from:prezto, nice:1
