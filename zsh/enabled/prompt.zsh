fb219eed2b57b62d69a9544b77606d3aff0aa11b
paulhybryant <paulhybryant@gmail.com>
2016-10-30 21:17:45 +0800

    .

--- zsh/enabled/prompt.zsh
+++ zsh/enabled/prompt.zsh
@@ -1,4 +1,5 @@
 # vim: filetype=zsh sw=2 ts=2 sts=2 et tw=80 foldlevel=0 nospell
 
 # Customization of powerlevel9k theme
-POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS-=(time)
+POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(context dir vcs)
+POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status background_jobs history)
