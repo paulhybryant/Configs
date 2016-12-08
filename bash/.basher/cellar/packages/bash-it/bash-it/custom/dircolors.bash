58ed7bcd06402f1d9e51bafb209864b8ef563a0b
paulhybryant <paulhybryant@gmail.com>
2016-12-08 14:14:24 +0800

    .

--- bash/.basher/cellar/packages/bash-it/bash-it/custom/dircolors.bash
+++ bash/.basher/cellar/packages/bash-it/bash-it/custom/dircolors.bash
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
+if [[ $OSTYPE == *darwin* ]]; then
+  alias dircolors=gdircolors
+  alias ls='gls --color=auto'
+fi
+
 pkg_path=$(basher package-path seebi/dircolors-solarized)
 eval $(dircolors $pkg_path/dircolors.256dark)
