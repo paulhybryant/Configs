343fcdd1a919585683fe868166058a03af8205c8
paulhybryant <paulhybryant@gmail.com>
2016-12-09 10:30:49 +0800

    .

--- zsh/.zplug.zsh
+++ zsh/.zplug.zsh
@@ -5,56 +5,54 @@ export ZPLUG_HOME="$HOME/.zplug"
   git clone --recursive https://github.com/zplug/zplug $ZPLUG_HOME
 source ${ZPLUG_HOME}/init.zsh
 
-zplug "zplug/zplug", nice:0
-
 zstyle ":prezto:environment:termcap" "color" "yes"
-zplug "modules/environment", from:prezto, nice:0
+zplug "modules/environment", from:prezto
 zstyle ":prezto:module:editor" "key-bindings" "vi"
-zplug "modules/editor", from:prezto, nice:1
-zplug "modules/directory", from:prezto, nice:1
-zplug "modules/helper", from:prezto, nice:1
-zplug "modules/history", from:prezto, nice:1
-zplug "modules/fasd", from:prezto, nice:1
-zplug "modules/osx", from:prezto, nice:1, if:"[[ $OSTYPE == *darwin* ]]"
-zplug "bhilburn/powerlevel9k", use:"powerlevel9k.zsh-theme", nice:2
-zplug "zsh-users/zsh-autosuggestions", nice:5
-zplug "zsh-users/zsh-completions", nice:5
-zplug "zsh-users/zsh-history-substring-search", nice:5
-zplug "willghatch/zsh-snippets", nice:5
-zplug "junegunn/fzf", use:"shell/*.zsh", nice:6
-zplug "zlsun/solarized-man", nice:6
-zplug "hlissner/zsh-autopair", nice:6
-zplug "urbainvaes/fzf-marks", nice:6
-zplug "unixorn/tumult.plugin.zsh", nice:6, if:"[[ $OSTYPE == *darwin* ]]"
-zplug "paulhybryant/dotfiles", nice:8
-zplug "psprint/zsh-syntax-highlighting", nice:10
+zplug "modules/editor", from:prezto
+zplug "modules/directory", from:prezto
+zplug "modules/helper", from:prezto
+zplug "modules/history", from:prezto
+zplug "modules/fasd", from:prezto
+zplug "modules/osx", from:prezto, if:"[[ $OSTYPE == *darwin* ]]"
+zplug "bhilburn/powerlevel9k", use:"powerlevel9k.zsh-theme"
+zplug "zsh-users/zsh-autosuggestions"
+zplug "zsh-users/zsh-completions"
+zplug "zsh-users/zsh-history-substring-search"
+zplug "willghatch/zsh-snippets"
+zplug "junegunn/fzf", use:"shell/*.zsh"
+zplug "zlsun/solarized-man"
+zplug "hlissner/zsh-autopair"
+zplug "urbainvaes/fzf-marks"
+zplug "unixorn/tumult.plugin.zsh", if:"[[ $OSTYPE == *darwin* ]]"
+zplug "paulhybryant/dotfiles"
+zplug "psprint/zsh-syntax-highlighting", defer:2
 
 # zstyle ":prezto:module:editor" "key-bindings" "vi"
-# zplug "modules/editor", from:prezto, nice:1
-# zplug "modules/ssh", from:prezto, nice:1
-# zplug "nojhan/liquidprompt", nice:2
-# zplug "mafredri/zsh-async", nice:3
-# zplug "supercrabtree/k", nice:5
-# zplug "pindexis/qfc", as:command, use:"bin/qfc", nice:5, hook-load:"source $ZPLUG_REPOS/pindexis/qfc/bin/qfc.sh"
-# zplug "aphelionz/strerror.plugin.zsh", nice:5
-# zplug "psprint/zsh-navigation-tools", nice:5
-# zplug "psprint/zsh-cmd-architect", nice:5
-# zplug "psprint/zsh-editing-workbench", nice:5
-# zplug "psprint/zsh-select", nice:5
-# zplug "psprint/zconvey", nice:5
-# zplug "psprint/history-search-multi-word", nice:5
-# zplug "psprint/ztrace", nice:5
-# zplug "psprint/zsnapshot", nice:5
-# zplug "zsh-users/zaw", nice:5
-# zplug "mooz/percol", use:"tools/zsh", nice:5
-# zplug "RobSis/zsh-completion-generator", nice:6
-# zplug "jocelynmallon/zshmarks", nice:6
-# zplug "paulhybryant/powerline-shell", nice:6
-# zplug "andrewferrier/fzf-z", nice:6
-# zplug "b4b4r07/enhancd", use:"init.sh", nice:7
-# zplug "zsh-users/zsh-syntax-highlighting", nice:10
-# zplug "trapd00r/zsh-syntax-highlighting-filetypes", nice:11
-# zplug "D630/fzf-fs", nice:5
+# zplug "modules/editor", from:prezto
+# zplug "modules/ssh", from:prezto
+# zplug "nojhan/liquidprompt"
+# zplug "mafredri/zsh-async"
+# zplug "supercrabtree/k"
+# zplug "pindexis/qfc", as:command, use:"bin/qfc", hook-load:"source $ZPLUG_REPOS/pindexis/qfc/bin/qfc.sh"
+# zplug "aphelionz/strerror.plugin.zsh"
+# zplug "psprint/zsh-navigation-tools"
+# zplug "psprint/zsh-cmd-architect"
+# zplug "psprint/zsh-editing-workbench"
+# zplug "psprint/zsh-select"
+# zplug "psprint/zconvey"
+# zplug "psprint/history-search-multi-word"
+# zplug "psprint/ztrace"
+# zplug "psprint/zsnapshot"
+# zplug "zsh-users/zaw"
+# zplug "mooz/percol", use:"tools/zsh"
+# zplug "RobSis/zsh-completion-generator"
+# zplug "jocelynmallon/zshmarks"
+# zplug "paulhybryant/powerline-shell"
+# zplug "andrewferrier/fzf-z"
+# zplug "b4b4r07/enhancd", use:"init.sh"
+# zplug "zsh-users/zsh-syntax-highlighting"
+# zplug "trapd00r/zsh-syntax-highlighting-filetypes"
+# zplug "D630/fzf-fs"
 
 [[ -f ~/.zplug.local ]] && source ~/.zplug.local                                # Local configurations
 
