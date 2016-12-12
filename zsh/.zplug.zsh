010bf8e29bfb682b4a7f142a9e868a34493bb5b9
paulhybryant <paulhybryant@gmail.com>
2016-12-12 11:31:11 +0800

    .

--- zsh/.zplug.zsh
+++ zsh/.zplug.zsh
@@ -29,8 +29,6 @@ zplug "paulhybryant/dotfiles", defer:1
 # zplug "psprint/zsh-syntax-highlighting", defer:2
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
 
-# zstyle ":prezto:module:editor" "key-bindings" "vi"
-# zplug "modules/editor", from:prezto
 # zplug "modules/ssh", from:prezto
 # zplug "nojhan/liquidprompt"
 # zplug "mafredri/zsh-async"
