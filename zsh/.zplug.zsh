def12514e84ae1db8dc455db742b72ca14853af4
paulhybryant <paulhybryant@gmail.com>
2016-12-12 09:38:25 +0800

    .

--- zsh/.zplug.zsh
+++ zsh/.zplug.zsh
@@ -26,7 +26,8 @@ zplug "hlissner/zsh-autopair", defer:1
 zplug "urbainvaes/fzf-marks", defer:1
 zplug "unixorn/tumult.plugin.zsh", if:"[[ $OSTYPE == *darwin* ]]", defer:1
 zplug "paulhybryant/dotfiles", defer:1
-zplug "psprint/zsh-syntax-highlighting", defer:2
+# zplug "psprint/zsh-syntax-highlighting", defer:2
+zplug "zsh-users/zsh-syntax-highlighting", defer:2
 
 # zstyle ":prezto:module:editor" "key-bindings" "vi"
 # zplug "modules/editor", from:prezto
