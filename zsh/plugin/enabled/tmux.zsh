c3073f23ab6e98e7be582616557c67537af8734a
paulhybryant <paulhybryant@gmail.com>
2016-12-21 12:10:32 +0800

    .

--- zsh/plugin/enabled/tmux.zsh
+++ zsh/plugin/enabled/tmux.zsh
@@ -1,7 +1,7 @@
 # vim: filetype=zsh sw=2 ts=2 sts=2 et tw=80 foldlevel=0 nospell
 
 zstyle ":registry:var:tmux-vars" registry \
-  "SSH_CLIENT" "SSH_OS" "SSH_AUTH_SOCK" "SSH_CONNECTIONS" \
+  "SSH_CLIENT" "SSH_OS" "SSH_AUTH_SOCK" "SSH_CONNECTION" \
   "DISPLAY" "SSH_AGENT_PID" "P4DIFF" "G4MULTIDIFF"
 
 alias tmux='TERM=screen-256color command tmux -2'
