0914240deb65cd5496be78e52d2bbaa6f00de3f5
paulhybryant <paulhybryant@gmail.com>
2016-12-27 12:19:34 +0800

    .

--- zsh/plugin/enabled/command-not-found.zsh
+++ zsh/plugin/enabled/command-not-found.zsh
@@ -1,3 +1,5 @@
 # vim: filetype=zsh sw=2 ts=2 sts=2 et tw=80 foldlevel=0 nospell
 
-eval "$(brew command-not-found-init)"
+if brew command command-not-found-init > /dev/null 2>&1; then
+  eval "$(brew command-not-found-init)"
+fi
