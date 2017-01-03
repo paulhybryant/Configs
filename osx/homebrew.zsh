9e93ebb9b27b41c7f7d2eb4fe5fe9c647cbbd7e0
paulhybryant <paulhybryant@gmail.com>
2017-01-03 15:48:04 +0800

    .

--- osx/homebrew.zsh
+++ osx/homebrew.zsh
@@ -16,19 +16,15 @@ brew tap paulhybryant/myformulae
 brew install macvim --with-override-system-vim --with-lua --with-luajit
 brew install reattach-to-user-namespace --with-wrap-launchctl --with-wrap-pbcopy-and-pbpaste
 brew install assh
-brew install homebrew/completions/brew-cask-completion
-brew install brew-gem
 brew install beeftornado/rmtree/brew-rmtree
+brew install brew-gem
 brew install cheat
-brew install homebrew/completions/docker-completion
 brew install dos2unix
 brew install fasd
 brew install findutils
 brew install fish
 brew install fpp
-brew install paulhybryant/myformulae/fsqlf
 brew install fzf
-brew install jhawthorn/fzy/fzy
 brew install git-extras
 brew install gnu-getopt
 brew install gnu-sed
@@ -36,9 +32,14 @@ brew install gnu-tar
 brew install gnu-which
 brew install graphviz
 brew install grc
-brew install htop
-brew install jq
+brew install hh
+brew install homebrew/completions/brew-cask-completion
+brew install homebrew/completions/docker-completion
+brew install homebrew/completions/vagrant-completion
 brew install homebrew/dupes/lsof
+brew install htop
+brew install jhawthorn/fzy/fzy
+brew install jq
 brew install mackup
 brew install macvim
 brew install mawk
@@ -52,12 +53,15 @@ brew install netcat
 brew install node
 brew install noti
 brew install osxutils
+brew install paulhybryant/myformulae/fsqlf
+brew install paulhybryant/myformulae/qfc
+brew install paulhybryant/myformulae/zunit
+brew install paulhybryant/myformulae/zvanilla
 brew install peco
 brew install percol
 brew install progress
 brew install proxychains-ng
 brew install python3
-brew install paulhybryant/myformulae/qfc
 brew install ranger
 brew install reattach-to-user-namespace
 brew install renameutils
@@ -71,11 +75,8 @@ brew install the_silver_searcher
 brew install tldr
 brew install tmux
 brew install urlview
-brew install homebrew/completions/vagrant-completion
 brew install xonsh
 brew install zshdb
-brew install paulhybryant/myformulae/zunit
-brew install paulhybryant/myformulae/zvanilla
 brew cask install docker
 brew cask install hyperswitch
 brew cask install karabiner
