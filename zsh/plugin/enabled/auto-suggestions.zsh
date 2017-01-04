0463cb4436440d6303d167d9b184f792533e4865
paulhybryant <paulhybryant@gmail.com>
2017-01-04 09:32:40 +0800

    .

--- zsh/plugin/enabled/auto-suggestions.zsh
+++ zsh/plugin/enabled/auto-suggestions.zsh
@@ -3,4 +3,4 @@
 # Otherwise the suggestions won't be visible with the default value.
 export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=240'
 
-bindkey '^I' autosuggest-accept
+bindkey '^N' autosuggest-accept
