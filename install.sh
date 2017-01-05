18c394b24a4a91263489a42de80980b999897860
paulhybryant <paulhybryant@gmail.com>
2017-01-05 11:06:26 +0800

    .

--- install.sh
+++ install.sh
@@ -2,7 +2,9 @@
 
 git clone https://github.com/zplug/zplug ~/.zplug
 git clone https://github.com/basherpm/basher.git ~/.basher
-mkdir -p ~/.pip ~/.ssh/assh.d ~/.cheat ~/.config/ranger ~/.local/bin ~/.tmux
+mkdir -p ~/.pip ~/.ssh/assh.d ~/.cheat ~/.config/ranger ~/.local/bin ~/.tmux ~/.vim/bundle
+git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+
 for module in bash misc tmux vim zsh; do
   stow $module -t ~
 done
@@ -14,3 +16,6 @@ else
 fi
 
 exec zsh
+
+vim -c 'NeoBundleInstall' -c 'q'
+~/.tmux/plugins/tpm/bin/install_plugins
