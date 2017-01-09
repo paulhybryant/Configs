a2250f3bd2a8b6fa57d05d1ee2486fb29c6b2b44
paulhybryant <paulhybryant@gmail.com>
2017-01-09 21:11:06 +0800

    .

--- zsh/.zplug.zsh
+++ zsh/.zplug.zsh
@@ -15,16 +15,20 @@ zplug "modules/fasd", from:prezto, defer:0
 zplug "modules/osx", from:prezto, defer:0, if:"[[ $OSTYPE == *darwin* ]]"
 
 # Must load zsh-cdr before zaw
+zplug "KasperChristensen/mylocation", defer:1
 zplug "Valodim/zsh-curl-completion", defer:1
 zplug "arzzen/calc.plugin.zsh", defer:1
 zplug "bhilburn/powerlevel9k", use:"powerlevel9k.zsh-theme", defer:1
+zplug "bric3/nice-exit-code", defer:1
 zplug "hchbaw/auto-fu.zsh", defer:1
 zplug "hlissner/zsh-autopair", defer:1
-# zplug "joepvd/grep2awk", defer:1
+zplug "joepvd/grep2awk", defer:1
 zplug "junegunn/fzf", use:"shell/*.zsh", defer:1
+zplug "oz/safe-paste", defer:1
+zplug "peterhurford/up.zsh", defer:1
 zplug "psprint/zsh-navigation-tools", defer:0
 zplug "seebi/dircolors-solarized", defer:1
-zplug "shannonmoeller/up", use:"up.sh", defer:1
+# zplug "shannonmoeller/up", use:"up.sh", defer:1
 zplug "unixorn/tumult.plugin.zsh", if:"[[ $OSTYPE == *darwin* ]]", defer:1
 zplug "urbainvaes/fzf-marks", defer:1
 zplug "willghatch/zsh-cdr", defer:1
@@ -42,7 +46,6 @@ zplug "zsh-users/zsh-syntax-highlighting", defer:3
 # zplug "D630/fzf-fs"
 # zplug "RobSis/zsh-completion-generator"
 # zplug "andrewferrier/fzf-z"
-# zplug "aphelionz/strerror.plugin.zsh"
 # zplug "arialdomartini/oh-my-git", use:"prompt.sh", defer:1
 # zplug "b4b4r07/enhancd", use:"init.sh"
 # zplug "huyng/bashmarks"
@@ -51,7 +54,6 @@ zplug "zsh-users/zsh-syntax-highlighting", defer:3
 # zplug "modules/ssh", from:prezto
 # zplug "mooz/percol", use:"tools/zsh"
 # zplug "nojhan/liquidprompt"
-# zplug "paulhybryant/powerline-shell"
 # zplug "pindexis/qfc", as:command, use:"bin/qfc", hook-load:"source $ZPLUG_REPOS/pindexis/qfc/bin/qfc.sh"
 # zplug "psprint/history-search-multi-word"
 # zplug "psprint/zconvey"
@@ -62,8 +64,6 @@ zplug "zsh-users/zsh-syntax-highlighting", defer:3
 # zplug "psprint/ztrace"
 # zplug "supercrabtree/k"
 # zplug "trapd00r/zsh-syntax-highlighting-filetypes"
-# zplug "zsh-users/zaw"
-# zplug "zsh-users/zsh-syntax-highlighting"
 
 [[ -f ~/.zplug.local ]] && source ~/.zplug.local                                # Local configurations
 
