46d79648e1e4a38f3ff9b973d01d7c11e2f31ad0
paulhybryant <paulhybryant@gmail.com>
2017-01-14 10:45:23 +0800

    .

--- zsh/plugin/enabled/iterm2.zsh
+++ zsh/plugin/enabled/iterm2.zsh
@@ -1,4 +1,5 @@
 # vim: ft=zsh sw=2 ts=2 sts=2 et tw=80 fdl=0 nospell
 
-test -e "${HOME}/.iterm2_shell_integration.zsh" && \
+if [[ $OSTYPE == *darwin* && -e "${HOME}/.iterm2_shell_integration.zsh"]]; then
   source "${HOME}/.iterm2_shell_integration.zsh"
+fi
