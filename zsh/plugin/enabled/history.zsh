9b2eca67c6ed5ad247cd0bbc4c84836ec498453a
paulhybryant <paulhybryant@gmail.com>
2017-01-19 16:23:13 +0800

    .

--- zsh/plugin/enabled/history.zsh
+++ zsh/plugin/enabled/history.zsh
@@ -1,12 +1,12 @@
 # vim: filetype=zsh sw=2 ts=2 sts=2 et tw=80 foldlevel=0 nospell
 
 declare -xg HIST_STAMPS='yyyy-mm-dd' HISTFILE="$HOME/.zsh_history"
-case $HIST_STAMPS in
-  'mm/dd/yyyy') alias history='fc -fl 1' ;;
-  'dd.mm.yyyy') alias history='fc -El 1' ;;
-  'yyyy-mm-dd') alias history='fc -il 1' ;;
-  *) alias history='fc -l 1' ;;
-esac
+# case $HIST_STAMPS in
+  # 'mm/dd/yyyy') alias history='fc -fl 1' ;;
+  # 'dd.mm.yyyy') alias history='fc -El 1' ;;
+  # 'yyyy-mm-dd') alias history='fc -il 1' ;;
+  # *) alias history='fc -l 1' ;;
+# esac
 # setopt INC_APPEND_HISTORY      # Provided by prezto history module, append history immediatly instead of wait until shell exits.
 setopt NO_APPEND_HISTORY
 setopt HIST_ALLOW_CLOBBER
