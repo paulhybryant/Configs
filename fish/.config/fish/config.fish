3075e44f994e3ed2490024471b80dbe6590b3f37
paulhybryant <paulhybryant@gmail.com>
2017-11-28 10:45:34 +0800

    .

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,7 @@
 # Plugins
 # fzf paths string edc/bass debug
 # omf/balias omf/brew omf/osx omf/pbcopy omf/peco omf/percol omf/ssh omf/tmux
-# omf/fasd
+# omf/fasd omf/theme-bobthefish
 
 # Aliases
 # alias wc "gwc"
@@ -11,8 +11,16 @@ alias j "z"
 abbr gcm "git commit -a -m"
 
 # Powerline prompt setup
-set fish_function_path $fish_function_path "$BREWHOME/lib/python2.7/site-packages/powerline/bindings/fish"
-powerline-setup
+# set fish_function_path $fish_function_path "$BREWHOME/lib/python2.7/site-packages/powerline/bindings/fish"
+# powerline-setup
+
+# omf/theme-bobthefish
+set -g theme_display_git_ahead_verbose yes
+set -g theme_display_hg yes
+set -g theme_display_cmd_duration yes
+set -g theme_show_exit_status yes
+set -g theme_color_scheme dark
+set -g theme_newline_cursor yes
 
 # Enable vi-mode
 fish_vi_key_bindings
