6aa2cc588a34ffe1fe688ce14b7c2251e1c7ae02
paulhybryant <paulhybryant@gmail.com>
2018-03-11 23:04:07 +0800

    .

--- linux/linuxbrew.zsh
+++ linux/linuxbrew.zsh
@@ -5,7 +5,11 @@ brew tap homebrew/core
 brew tap linuxbrew/extra
 brew tap linuxbrew/xorg
 brew tap martido/brew-graph
+brew install aria2
 brew install assh
+brew install ccat
+brew install cmake
+brew install expat
 brew install fasd
 brew install fisherman/tap/fisherman
 brew install fzf
@@ -15,15 +19,29 @@ brew install gnu-getopt
 brew install gnu-sed
 brew install gnu-tar
 brew install gnu-which
+brew install go
 brew install gpatch
+brew install htop
+brew install jq
+brew install lsof
 brew install martido/brew-graph/brew-graph
+brew install moreutils
 brew install most
 brew install node
+brew install noti
 brew install patchelf  --with-static
+brew install peco
+brew install percol
 brew install pkg-config
+brew install psgrep
+brew install pstree
+brew install ranger
 brew install shc
 brew install sshrc
 brew install stow
 brew install the_silver_searcher
+brew install tig
+brew install tldr
 brew install tmux
-brew install zsh
+brew install wget
+brew install zsh  --with-pcre
