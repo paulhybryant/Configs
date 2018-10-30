f5cb78cce0ead5367f90a12733584f5ee9c86c8d
paulhybryant <paulhybryant@gmail.com>
2018-10-30 11:50:11 +0800

    Cleanup brew installations.

--- linux/linuxbrew.zsh
+++ linux/linuxbrew.zsh
@@ -10,76 +10,47 @@ brew tap linuxbrew/xorg
 brew tap martido/brew-graph
 brew tap paulhybryant/myformulae
 brew tap sebglazebrook/aliases
-brew install aria2
 brew install assh
-brew install bash
 brew install brew-gem
 brew install ccat
-brew install cheat
-brew install check
 brew install cmake
-brew install docker-completion
+brew install coreutils
+brew install curl
 brew install dos2unix
 brew install fasd
 brew install findutils
-brew install fisherman
-brew install fpp
 brew install fzf
-brew install gcc
 brew install git-extras
 brew install gnu-getopt
 brew install gnu-sed
 brew install gnu-tar
 brew install gnu-which
-brew install gpatch
-brew install graphviz
 brew install grc
-brew install hh
-brew install hr
 brew install htop
-brew install httpie
-brew install jhawthorn/fzy/fzy
 brew install jq
 brew install linuxbrew/xorg/util-macros
 brew install linuxbrew/xorg/xorg
 brew install lsof
 brew install lua@5.1
 brew install martido/brew-graph/brew-graph
-brew install mawk
-brew install mdp
 brew install moreutils
 brew install most
-brew install neovim
 brew install netcat
 brew install nmap
-brew install node
 brew install noti
 brew install patchelf  --with-static
 brew install --HEAD paulhybryant/myformulae/fsqlf
 brew install --HEAD paulhybryant/myformulae/powerline-fonts
-brew install --HEAD paulhybryant/myformulae/qfc
 brew install paulhybryant/myformulae/xbindkeys
-brew install --HEAD paulhybryant/myformulae/zunit
-brew install --HEAD paulhybryant/myformulae/zvanilla
 brew install peco
 brew install percol
-brew install progress
 brew install psgrep
 brew install pstree
 brew install ranger
-brew install renameutils
-brew install shc
 brew install sshrc
-brew install stdman
 brew install stow
 brew install the_silver_searcher
-brew install tig
-brew install tldr
 brew install tmux
-brew install todo-txt
-brew install tree
-brew install urlview
 brew install vim  --with-override-system-vi --with-client-server --with-luajit
 brew install wget
-brew install xonsh
-brew install zshdb
+brew install zsh  --with-pcre
