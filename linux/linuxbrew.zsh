46411829967ed6872734198b0b635980dd522e28
paulhybryant <paulhybryant@gmail.com>
2018-10-31 10:47:52 +0800

    Cleanup of linuxbrew packages.

--- linux/linuxbrew.zsh
+++ linux/linuxbrew.zsh
@@ -13,41 +13,15 @@ brew tap sebglazebrook/aliases
 brew install assh
 brew install brew-gem
 brew install ccat
-brew install cmake
-brew install coreutils
-brew install curl
 brew install fasd
-brew install findutils
 brew install fzf
 brew install git-extras
-brew install gnu-getopt
-brew install gnu-sed
-brew install gnu-tar
-brew install gnu-which
-brew install grc
-brew install htop
-brew install jq
-brew install linuxbrew/xorg/xorg
-brew install lsof
 brew install martido/brew-graph/brew-graph
-brew install moreutils
-brew install most
-brew install netcat
-brew install nmap
 brew install noti
-brew install patchelf  --with-static
 brew install --HEAD paulhybryant/myformulae/fsqlf
 brew install --HEAD paulhybryant/myformulae/powerline-fonts
-brew install paulhybryant/myformulae/xbindkeys
-brew install peco
-brew install percol
 brew install psgrep
 brew install pstree
-brew install ranger
 brew install sshrc
-brew install stow
-brew install the_silver_searcher
 brew install tmux
-brew install vim  --with-override-system-vi --with-client-server --with-luajit
-brew install wget
 brew install zsh  --with-pcre
