7fd9f48e42a02ee31758c5408a69085a1fb6986b
paulhybryant <paulhybryant@gmail.com>
2018-12-21 11:52:11 +0800

    .

--- qnap/qnapware_install_arm.sh
+++ qnap/qnapware_install_arm.sh
@@ -1,43 +1,46 @@
 #!/bin/sh
 
 . /etc/init.d/functions
-QNAPWARE_DIR=${1:-"${DEF_VOLMP}/@Qnapware"}
+FindDefVol
+mkdir ${DEF_VOLMP}/@Qnapware
 
 echo "Info: Checking for prerequisites and creating folders..."
-if [ -d ${QNAPWARE_DIR} ]
+if [ -d /Apps ]
 then
-    echo "Warning: Folder ${QNAPWARE_DIR} exists!"
+    echo "Warning: Folder /Apps exists!"
+    exit 1
 else
-    mkdir ${QNAPWARE_DIR}
+    mkdir /Apps
+    mount -o bind ${DEF_VOLMP}/@Qnapware /Apps
 fi
-if [ -d ${QNAPWARE_DIR}/opt ]
+if [ -d /Apps/opt ]
 then
-    echo "Warning: Folder ${QNAPWARE_DIR}/opt exists!"
+    echo "Warning: Folder /Apps/opt exists!"
 else
-    mkdir ${QNAPWARE_DIR}/opt
+    mkdir /Apps/opt
 fi
 for folder in bin etc include lib sbin share tmp usr var
 do
-  if [ -d "${QNAPWARE_DIR}/opt/$folder" ]
+  if [ -d "/Apps/opt/$folder" ]
   then
-    echo "Warning: Folder ${QNAPWARE_DIR}/opt/$folder exists!"
-    echo "Warning: If something goes wrong please clean ${QNAPWARE_DIR}/opt folder and try again."
+    echo "Warning: Folder /Apps/opt/$folder exists!"
+    echo "Warning: If something goes wrong please clean /opt folder and try again."
   else
-    mkdir ${QNAPWARE_DIR}/opt/$folder
+    mkdir /Apps/opt/$folder
   fi
 done
-[ -d "${QNAPWARE_DIR}/opt/lib/opkg" ] || mkdir -p ${QNAPWARE_DIR}/opt/lib/opkg
-[ -d "${QNAPWARE_DIR}/opt/var/lock" ] || mkdir -p ${QNAPWARE_DIR}/opt/var/lock
-[ -d "${QNAPWARE_DIR}/opt/var/log" ] || mkdir -p ${QNAPWARE_DIR}/opt/var/log
-[ -d "${QNAPWARE_DIR}/opt/var/run" ] || mkdir -p ${QNAPWARE_DIR}/opt/var/run
+[ -d "/Apps/opt/lib/opkg" ] || mkdir -p /Apps/opt/lib/opkg
+[ -d "/Apps/opt/var/lock" ] || mkdir -p /Apps/opt/var/lock
+[ -d "/Apps/opt/var/log" ] || mkdir -p /Apps/opt/var/log
+[ -d "/Apps/opt/var/run" ] || mkdir -p /Apps/opt/var/run
 
 echo "Info: Opkg package manager deployment..."
-cd ${QNAPWARE_DIR}/opt/bin
+cd /Apps/opt/bin
 wget http://qnapware.zyxmon.org/binaries-arm/installer/opkg
-chmod +x ${QNAPWARE_DIR}/opt/bin/opkg
-cd ${QNAPWARE_DIR}/opt/etc
+chmod +x /Apps/opt/bin/opkg
+cd /Apps/opt/etc
 wget http://qnapware.zyxmon.org/binaries-arm/installer/opkg.conf
-cd ${QNAPWARE_DIR}/opt/lib
+cd /Apps/opt/lib
 wget http://qnapware.zyxmon.org/binaries-arm/installer/ld-2.20.so
 chmod +x ld-2.20.so
 ln -s ld-2.20.so ld-linux.so.3
@@ -45,11 +48,11 @@ wget http://qnapware.zyxmon.org/binaries-arm/installer/libc-2.20.so
 ln -s libc-2.20.so libc.so.6
 
 echo "Info: Basic packages installation..."
-${QNAPWARE_DIR}/opt/bin/opkg update
-${QNAPWARE_DIR}/opt/bin/opkg install qnapware
+/Apps/opt/bin/opkg update
+/Apps/opt/bin/opkg install qnapware
 
 echo "Info: Congratulations!"
 echo "Info: If there are no errors above then Entware successfully initialized."
-echo "Info: Add ${QNAPWARE_DIR}/opt/bin & ${QNAPWARE_DIR}/opt/sbin to your PATH variable"
-echo "Info: Add '${QNAPWARE_DIR}/opt/etc/init.d/rc.unslung start' to startup script for qnapware services to start"
+echo "Info: Add /Apps/opt/bin & /Apps/opt/sbin to your PATH variable"
+echo "Info: Add '/Apps/opt/etc/init.d/rc.unslung start' to startup script for qnapware services to start"
 echo "Info: Found a Bug? Please report at https://github.com/Entware/entware/issues"
