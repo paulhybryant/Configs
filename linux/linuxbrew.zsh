660f8b12f5b6b0ddc00d794935f514594f0e9e42
paulhybryant <paulhybryant@gmail.com>
2019-03-22 11:33:56 +0800

    .

--- linux/linuxbrew.zsh
+++ linux/linuxbrew.zsh
@@ -11,22 +11,27 @@ brew tap martido/brew-graph
 brew tap paulhybryant/myformulae
 brew tap sebglazebrook/aliases
 brew install assh
+brew install bower
 brew install brew-gem
 brew install brew-pip
 brew install ccat
-brew install curl
 brew install docker-completion
 brew install fasd
 brew install fzf
+brew install git
 brew install git-extras
 brew install go
+brew install grunt-cli
 brew install martido/brew-graph/brew-graph
 brew install noti
 brew install patchelf  --with-static
+brew install --HEAD paulhybryant/myformulae/aria2-daemon
 brew install --HEAD paulhybryant/myformulae/powerline-fonts
 brew install pkg-config
 brew install psgrep
 brew install pstree
+brew install python
+brew install python@2
 brew install q
 brew install sshrc
 brew install tmux
