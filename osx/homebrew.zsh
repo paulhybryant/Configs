6825197bd07f459db0671a69a1c13c01d1d86025
paulhybryant <paulhybryant@gmail.com>
2020-07-31 10:49:58 +0800

    .

--- osx/homebrew.zsh
+++ osx/homebrew.zsh
@@ -24,16 +24,15 @@ brew install brew-gem
 brew install brew-pip
 brew install ccat
 brew install coreutils
+brew install csvq
 brew install curl
 brew install docker-completion
 brew install e2fsprogs
 brew install fasd
 brew install findutils
 brew install fzf
-brew install ghostscript
 brew install git
 brew install git-extras
-brew install gnu-getopt
 brew install gnu-sed
 brew install gnu-tar
 brew install gnu-which
@@ -42,7 +41,6 @@ brew install grc
 brew install grunt-cli
 brew install htop
 brew install imagemagick
-brew install koekeishiya/formulae/khd
 brew install koekeishiya/formulae/skhd
 brew install koekeishiya/formulae/yabai
 brew install lsof
@@ -59,26 +57,25 @@ brew install pandoc
 brew install --HEAD paulhybryant/myformulae/aria2-daemon
 brew install --HEAD paulhybryant/myformulae/fsqlf
 brew install --HEAD paulhybryant/myformulae/powerline-fonts
+brew install paulhybryant/myformulae/sshrc
 brew install peco
 brew install percol
 brew install pkg-config
 brew install psgrep
 brew install pstree
 brew install python-yq
-brew install python@2
 brew install q
 brew install ranger
 brew install reattach-to-user-namespace
 brew install sebglazebrook/aliases/aliases
-brew install sshrc
 brew install stow
 brew install telnet
 brew install the_silver_searcher
 brew install tmux
 brew install trash-cli
 brew install uni2ascii
-brew install waltarix/customs/gnu-cpio
 brew install wget
+brew install you-get
 brew install yudai/gotty/gotty
 brew install zsh
 brew cask install alfred
@@ -91,7 +88,6 @@ brew cask install firefox
 brew cask install flux
 brew cask install font-awesome-terminal-fonts
 brew cask install font-hack-nerd-font
-brew cask install freemind
 brew cask install google-backup-and-sync
 brew cask install hyperswitch
 brew cask install iterm2
@@ -102,17 +98,12 @@ brew cask install kindle
 brew cask install knotes
 brew cask install libreoffice
 brew cask install macvim
-brew cask install mubu
 brew cask install nextcloud
-brew cask install opera
 brew cask install osxfuse
 brew cask install plistedit-pro
 brew cask install prefs-editor
-brew cask install qownnotes
 brew cask install sogouinput
 brew cask install spectacle
-brew cask install vanilla
 brew cask install wechat
-brew cask install xmind-zen
 brew cask install xquartz
 brew cask install yinxiangbiji
